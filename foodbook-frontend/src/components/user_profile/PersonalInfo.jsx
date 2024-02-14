5c63eb02f1e7dea6a6a35465ed047a7116b16ce8
AldinHyuseinov <Aldin6687@gmail.com>
2024-02-14 18:47:30 +0200

    some work on PersonalInfo component

--- foodbook-frontend/src/components/user_profile/PersonalInfo.jsx
+++ foodbook-frontend/src/components/user_profile/PersonalInfo.jsx
@@ -1,8 +1,41 @@
 import { getUserData } from "../../services/userService";
 import "../../assets/css/form/form.css";
 import "../../assets/css/user_profile/personal-info.css";
+import { useState } from "react";
+import useButtonState from "../../hooks/useButtonState";
 
 export default function PersonalInfo() {
+  const [submitButtonDisabled, handleButtonState] = useButtonState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Todo
+  };
+
+  const handleFirstNameChange = (e) => {
+    const value = e.target.value;
+
+    handleButtonState([value, lastName, birthDate]);
+    setFirstName(value);
+  };
+
+  const handleLastNameChange = (e) => {
+    const value = e.target.value;
+
+    handleButtonState([firstName, value, birthDate]);
+    setLastName(value);
+  };
+
+  const handleBirthDateChange = (e) => {
+    const value = e.target.value;
+
+    handleButtonState([firstName, lastName, value]);
+    setBirthDate(value);
+  };
+
   return (
     <div className="personal-info">
       <header>
@@ -19,7 +52,7 @@ export default function PersonalInfo() {
         <section className="personal-info-section">
           <h2>My Basic Info</h2>
 
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-input">
               <label htmlFor="email">Email Address</label>
               <input type="email" id="email" readOnly value={getUserData().email} />
@@ -27,19 +60,31 @@ export default function PersonalInfo() {
 
             <div className="form-input">
               <label htmlFor="first-name">First Name</label>
-              <input type="text" id="first-name" />
+              <input
+                type="text"
+                id="first-name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+              />
             </div>
 
             <div className="form-input">
               <label htmlFor="last-name">Last Name</label>
-              <input type="text" id="last-name" />
+              <input type="text" id="last-name" value={lastName} onChange={handleLastNameChange} />
             </div>
 
             <div className="form-input">
               <label htmlFor="birth-date">Birth Date</label>
-              <input type="date" id="birth-date" />
+              <input
+                type="date"
+                id="birth-date"
+                value={birthDate}
+                onChange={handleBirthDateChange}
+              />
             </div>
-            <button type="submit">Save</button>
+            <button type="submit" disabled={submitButtonDisabled}>
+              Save
+            </button>
           </form>
         </section>
       </main>
