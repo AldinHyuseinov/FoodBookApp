d418fcd636d1d049d7688d5c8eb22cba5378d107
AldinHyuseinov <Aldin6687@gmail.com>
2024-10-13 15:04:21 +0300

    better summary error

--- foodbook-frontend/src/components/just_recipe/RecipeSummaryError.jsx
+++ foodbook-frontend/src/components/just_recipe/RecipeSummaryError.jsx
@@ -1,7 +1,21 @@
+import "../../assets/css/recipe_extract/recipe-summary-error.css";
+
 export default function RecipeSummaryError() {
   return (
-    <main>
-      <p>Error!</p>
-    </main>
+    <div className="page-container">
+      <header>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"
+            fill="orange"
+          />
+        </svg>
+        <h1 className="no-recipe">No recipe found!</h1>
+      </header>
+
+      <main>
+        <p>This often happens when a website&apos;s code is poorly formatted.</p>
+      </main>
+    </div>
   );
 }
